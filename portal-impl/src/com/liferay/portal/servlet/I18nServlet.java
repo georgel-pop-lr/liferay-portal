8e5e70b97cc8fb1f9af38f2a67c9342a24076ddd
tina.tian <tina.tian@liferay.com>
2021-04-22 08:56:19 -0700

    LPS-130105 Use FriendlyURLNormalizerUtil#normalizeWithEncoding to ensure we always get the correct encoded friendly url

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -25,8 +25,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -215,7 +215,7 @@ public class I18nServlet extends HttpServlet {
 			}
 		}
 
-		String redirect = HttpUtil.encodePath(path);
+		String redirect = FriendlyURLNormalizerUtil.normalizeWithEncoding(path);
 
 		if (_log.isDebugEnabled()) {
 			_log.debug("Redirect " + redirect);
