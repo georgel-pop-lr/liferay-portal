bbd78e4466e6d548b499032f079d82e4d7d56a7b
Georgel Pop <georgel.pop@liferay.com>
2025-09-15 14:04:10 +0200

    LPD-63444 Adapt methods usages

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/upgrade/v2_9_4/test/FragmentEntryLinkUpgradeProcessTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/upgrade/v2_9_4/test/FragmentEntryLinkUpgradeProcessTest.java
@@ -166,7 +166,8 @@ public class FragmentEntryLinkUpgradeProcessTest
 				JSONUtil.put(
 					RandomTestUtil.randomString(),
 					RandomTestUtil.randomString())
-			).toString());
+			).toString(),
+			true);
 	}
 
 	private void _assertFragmentEntryLinksType(
