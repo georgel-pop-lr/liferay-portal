ab9c63125f1026b827f89dad22dc1cc3941d8cc4
Georgel Pop <georgel.pop@liferay.com>
2026-03-20 17:07:32 +0100

    LPD-63086 Adapt tests for FragmentSetModal single-fragment imports

--- modules/apps/layout/layout-js-components-web/test/components/import/Import.test.js
+++ modules/apps/layout/layout-js-components-web/test/components/import/Import.test.js
@@ -4,12 +4,15 @@
  */
 
 import '@testing-library/jest-dom';
-import {fireEvent, render} from '@testing-library/react';
+import {fireEvent, render, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {navigate} from 'frontend-js-web';
 import React from 'react';
 
 import {Import} from '../../../src/main/resources/META-INF/resources/js';
+import importZipFile from '../../../src/main/resources/META-INF/resources/js/components/import/importZipFile';
+import FragmentSetModal from '../../../src/main/resources/META-INF/resources/js/components/modals/FragmentSetModal';
+import openModalComponent from '../../../src/main/resources/META-INF/resources/js/components/modals/openModalComponent';
 
 jest.mock('frontend-js-web', () => ({
 	...jest.requireActual('frontend-js-web'),
@@ -17,15 +20,30 @@ jest.mock('frontend-js-web', () => ({
 	navigate: jest.fn(),
 }));
 
+jest.mock(
+	'../../../src/main/resources/META-INF/resources/js/components/import/importZipFile',
+	() => jest.fn(() => Promise.resolve())
+);
+
+jest.mock(
+	'../../../src/main/resources/META-INF/resources/js/components/modals/openModalComponent'
+);
+
 function renderComponent({
+	addFragmentCollectionURL = '/o/test/add_fragment_collection',
 	backURL = 'backURL',
+	fragmentCollectionId,
+	fragmentCollections = [{fragmentCollectionId: 1, name: 'Set Name'}],
 	helpLink,
 	importURL = 'importURL',
 	portletNamespace = 'namespace',
 } = {}) {
 	return render(
 		<Import
+			addFragmentCollectionURL={addFragmentCollectionURL}
 			backURL={backURL}
+			fragmentCollectionId={fragmentCollectionId}
+			fragmentCollections={fragmentCollections}
 			helpLink={helpLink}
 			importURL={importURL}
 			portletNamespace={portletNamespace}
@@ -109,21 +127,113 @@ describe('Import', () => {
 		expect(await findByText('Learn more')).toBeInTheDocument();
 	});
 
-	it.skip('renders Import Options modal', async () => {
-		const {findByLabelText, findByRole, findByText} = renderComponent();
+	it('opens fragment set modal for single fragment imports', async () => {
+		importZipFile.mockImplementation(({handleResponse}) => {
+			handleResponse(
+				{
+					hasConflicts: false,
+					importResults: {},
+					needsFragmentCollection: true,
+				},
+				new File(['(⌐□_□)'], 'example.zip', {
+					type: 'application/zip',
+				})
+			);
 
-		const button = await findByRole('button', {name: /import/i});
+			return Promise.resolve();
+		});
+
+		const {findByLabelText, findByRole} = renderComponent();
 
 		const file = new File(['(⌐□_□)'], 'example.zip', {
-			type: 'image/png',
+			type: 'application/zip',
 		});
 
-		fireEvent.change(await findByLabelText('file-upload'), {
-			target: {files: [file]},
+		await userEvent.upload(await findByLabelText('file-upload'), file);
+		await userEvent.click(await findByRole('button', {name: /import/i}));
+
+		expect(openModalComponent).toHaveBeenCalledWith(
+			expect.objectContaining({
+				ModalComponent: FragmentSetModal,
+			})
+		);
+	});
+
+	it('retries import with selected fragment collection after fragment set modal submission', async () => {
+		importZipFile.mockImplementation(
+			({fragmentCollectionId, handleResponse}) => {
+				handleResponse(
+					{
+						hasConflicts: false,
+						importResults:
+							fragmentCollectionId === 1
+								? {'fragment-id': 'Fragment Name'}
+								: {},
+						needsFragmentCollection: fragmentCollectionId !== 1,
+					},
+					new File(['(⌐□_□)'], 'example.zip', {
+						type: 'application/zip',
+					})
+				);
+
+				return Promise.resolve();
+			}
+		);
+
+		const {findByLabelText, findByRole} = renderComponent();
+
+		const file = new File(['(⌐□_□)'], 'example.zip', {
+			type: 'application/zip',
 		});
 
-		expect(button).not.toBeDisabled();
+		await userEvent.upload(await findByLabelText('file-upload'), file);
+		await userEvent.click(await findByRole('button', {name: /import/i}));
 
-		expect(await findByText('import-options')).toBeInTheDocument();
+		const {modalComponentProps} = openModalComponent.mock.calls[0][0];
+
+		await modalComponentProps.onSubmitFragmentCollection(1);
+
+		await waitFor(() => {
+			expect(importZipFile).toHaveBeenLastCalledWith(
+				expect.objectContaining({
+					fragmentCollectionId: 1,
+				})
+			);
+		});
+	});
+
+	it('does not open fragment set modal when the import view already has a selected collection', async () => {
+		importZipFile.mockImplementation(({handleResponse}) => {
+			handleResponse(
+				{
+					hasConflicts: false,
+					importResults: {'fragment-id': 'Fragment Name'},
+					needsFragmentCollection: false,
+				},
+				new File(['(⌐□_□)'], 'example.zip', {
+					type: 'application/zip',
+				})
+			);
+
+			return Promise.resolve();
+		});
+
+		const {findByLabelText, findByRole} = renderComponent({
+			fragmentCollectionId: 1,
+		});
+
+		const file = new File(['(⌐□_□)'], 'example.zip', {
+			type: 'application/zip',
+		});
+
+		await userEvent.upload(await findByLabelText('file-upload'), file);
+		await userEvent.click(await findByRole('button', {name: /import/i}));
+
+		expect(openModalComponent).not.toHaveBeenCalled();
+		expect(importZipFile).toHaveBeenCalledWith(
+			expect.objectContaining({
+				fragmentCollectionId: 1,
+			})
+		);
 	});
 });
