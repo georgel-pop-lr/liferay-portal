66e1596a782124630bff679bba672e02b629994c
Georgel Pop <georgel.pop@liferay.com>
2026-06-24 11:52:27 +0200

    LPD-94450 Add the Audience Builder attributes sidebar

--- modules/apps/audiences/audiences-web/src/main/resources/META-INF/resources/js/AudienceBuilder.tsx
+++ modules/apps/audiences/audiences-web/src/main/resources/META-INF/resources/js/AudienceBuilder.tsx
@@ -9,17 +9,25 @@ import ClayIcon from '@clayui/icon';
 import ClayLayout from '@clayui/layout';
 import ClayLink from '@clayui/link';
 import ClayToolbar from '@clayui/toolbar';
-import React, {useState} from 'react';
+import {cancelDebounce, debounce} from 'frontend-js-web';
+import React, {useEffect, useRef, useState} from 'react';
+
+import AttributesSidebar from './components/AttributesSidebar';
+import {AudiencesCriteriaType} from './types';
 
 const NAME_MAX_LENGTH = 75;
 
+const SIDEBAR_WIDTH = '25%';
+
 interface IProps {
+	audiencesCriteriaTypes?: AudiencesCriteriaType[];
 	backURL?: string;
 	name?: string;
 	namespace?: string;
 }
 
 export default function AudienceBuilder({
+	audiencesCriteriaTypes = [],
 	backURL,
 	name,
 	namespace = '',
@@ -27,9 +35,45 @@ export default function AudienceBuilder({
 	const [currentName, setCurrentName] = useState(
 		name || Liferay.Language.get('new-audience')
 	);
+	const [height, setHeight] = useState<string>();
+
+	const editorRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const editor = editorRef.current;
+
+		if (!editor) {
+			return;
+		}
+
+		const updateHeight = () => {
+			setHeight(
+				`${Math.max(
+					0,
+					window.innerHeight - editor.getBoundingClientRect().top
+				)}px`
+			);
+		};
+
+		updateHeight();
+
+		const debouncedUpdateHeight = debounce(updateHeight, 100);
+
+		window.addEventListener('resize', debouncedUpdateHeight);
+
+		return () => {
+			cancelDebounce(debouncedUpdateHeight);
+
+			window.removeEventListener('resize', debouncedUpdateHeight);
+		};
+	}, []);
 
 	return (
-		<>
+		<div
+			className="d-flex flex-column overflow-hidden"
+			ref={editorRef}
+			style={{height}}
+		>
 			<ClayToolbar>
 				<ClayLayout.ContainerFluid size={false}>
 					<ClayToolbar.Nav>
@@ -77,12 +121,17 @@ export default function AudienceBuilder({
 				</ClayLayout.ContainerFluid>
 			</ClayToolbar>
 
-			<ClayLayout.Row className="m-0">
-				<ClayLayout.Col className="border-right p-4" md={3}>
-					<h4>{Liferay.Language.get('attributes-types')}</h4>
-				</ClayLayout.Col>
+			<div className="d-flex flex-grow-1 min-h-0">
+				<div
+					className="border-right d-flex flex-column px-4"
+					style={{width: SIDEBAR_WIDTH}}
+				>
+					<AttributesSidebar
+						audiencesCriteriaTypes={audiencesCriteriaTypes}
+					/>
+				</div>
 
-				<ClayLayout.Col className="p-4" md={9}>
+				<div className="flex-grow-1 min-h-0 overflow-auto p-4">
 					<ClayForm.Group>
 						<label htmlFor={`${namespace}name`}>
 							{Liferay.Language.get('name')}
@@ -104,8 +153,8 @@ export default function AudienceBuilder({
 							value={currentName}
 						/>
 					</ClayForm.Group>
-				</ClayLayout.Col>
-			</ClayLayout.Row>
-		</>
+				</div>
+			</div>
+		</div>
 	);
 }
